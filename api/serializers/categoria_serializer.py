428730ac1f1e7b705c768d4a4d409270b0f25f1e
Fernando Antunes <f.antunes84@gmail.com>
2022-07-12 14:24:07 -0300

    API: Removido o retorno de categorias relacionadas dentro de subcategorias.

--- api/serializers/categoria_serializer.py
+++ api/serializers/categoria_serializer.py
@@ -7,14 +7,3 @@ class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
         fields = ['tipo', 'cor', 'descricao', 'usuario']
-
-    def __init__(self, *args, **kwargs):
-        fields = kwargs.pop('fields', None)
-
-        super().__init__(*args, **kwargs)
-
-        if fields is not None:
-            allowed = set(fields)
-            existing = set(self.fields)
-            for fild_name in existing - allowed:
-                self.fields.pop(fild_name)
--- api/serializers/subcategoria_serializer.py
+++ api/serializers/subcategoria_serializer.py
@@ -5,19 +5,6 @@ from balanco.models import Subcategoria
 
 
 class SubcategoriaSerializer(serializers.ModelSerializer):
-    categoria = CategoriaSerializer(fields=('id', 'descricao'))
-
     class Meta:
         model = Subcategoria
         fields = ['id', 'descricao', 'categoria']
-
-    def __init__(self, *args, **kwargs):
-        fields = kwargs.pop('fields', None)
-
-        super().__init__(*args, **kwargs)
-
-        if fields is not None:
-            allowed = set(fields)
-            existing = set(self.fields)
-            for field_name in existing - allowed:
-                self.fields.pop(field_name)
