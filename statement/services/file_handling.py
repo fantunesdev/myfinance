3e74570b1ceeb9a96fb4fa5f309036e896ea536b
Fernando Antunes <f.antunes84@gmail.com>
2023-12-29 23:59:17 -0300

    Início importação de planilhas

--- statement/services/file_handling.py
+++ statement/services/file_handling.py
@@ -7,15 +7,28 @@ class FileHandling():
         self.file = file
         self.extention = file.name.split('.')[-1].lower()
         self.path = os.path.join(settings.MEDIA_ROOT, 'uploads', file.name)
+        self.__handle_file()
+
+    def __handle_file(self):
         self.__save_file()
+        content = self.__read_file()
+        print(content)
+        self.__remove_file()
 
     def __save_file(self):
-        print(self.extention)
         with open(self.path, 'wb+') as destination:
             for chunk in self.file.chunks():
                 destination.write(chunk)
 
-    def read_file(self):
-        with open(self.path, 'r') as file:
-            content = file.read()
-            print(content)
+    def __read_file(self):
+        if self.extention == 'txt':
+            with open(self.path, 'r') as file:
+                content = file.read()
+            return content
+    
+    def __remove_file(self):
+        try:
+            os.remove(self.path)
+            print(f'Arquivo {self.file.name} removido.')
+        except FileNotFoundError:
+            print(f'Arquivo {self.file.name} não encontrado.')
