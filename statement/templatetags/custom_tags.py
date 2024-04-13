1f7268f872e8c8cf7f9a777ec179f866a32f72b2
Fernando Antunes <f.antunes84@gmail.com>
2024-04-13 15:14:52 -0300

    🚑 Ajuste nos cálculos de meses restantes

--- statement/templatetags/custom_tags.py
+++ statement/templatetags/custom_tags.py
@@ -1,5 +1,4 @@
-import datetime
-
+from datetime import date
 from dateutil.relativedelta import relativedelta
 from django import template
 from django.utils.formats import number_format
@@ -27,8 +26,13 @@ def get_remaining_value_for_dream(dream_id, dream_value, user):
 @register.simple_tag
 def calcule_remaining_installment_value(dream_id, dream_value, dream_limit_date, user):
     remaining_value = portion_services.calculate_remaining_value(dream_id, dream_value, user)
-    today = datetime.datetime.today()
-    remaining_months = relativedelta(dream_limit_date - today).months
+    today = date.today()
+    if dream_limit_date > today:
+        diference_months_in_years = (dream_limit_date.year - today.year) * 12
+        difference_months = dream_limit_date.month - today.month
+        remaining_months = diference_months_in_years + difference_months
+    else:
+        remaining_months = 1
     remaining_limit_date = remaining_value / remaining_months
     formatted_total = number_format(remaining_limit_date, decimal_pos=2, force_grouping=True)
     return f'R$ {formatted_total}'
@@ -36,5 +40,9 @@ def calcule_remaining_installment_value(dream_id, dream_value, dream_limit_date,
 
 @register.simple_tag
 def calculate_remaining_months(dream):
-    today = datetime.datetime.today()
-    return dream.limit_date.month - today.month
+    today = date.today()
+    diference_months_in_years = (dream.limit_date.year - today.year) * 12
+    difference_months = dream.limit_date.month - today.month
+    if difference_months > 0:
+        return diference_months_in_years + difference_months
+    return 0
