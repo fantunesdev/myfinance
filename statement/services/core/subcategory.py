fefda95dfcaaa886eaac736968a1bcb99c20fd29
Fernando Antunes <f.antunes84@gmail.com>
2025-03-13 10:14:42 -0300

    🐛 Solução do bug ao consultar subcategorias de categoria

--- statement/services/core/subcategory.py
+++ statement/services/core/subcategory.py
@@ -6,5 +6,9 @@ class SubcategoryService(BaseService):
     """Serviço para gerenciar operações relacionadas ao modelo Subcategory."""
     model = Subcategory
 
-    def get_by_category(self, category_id):
+    @staticmethod
+    def get_by_category(category_id):
+        """
+        Obtém as subcategorias de uma categoria
+        """
         return Subcategory.objects.select_related('category').filter(category=category_id)
