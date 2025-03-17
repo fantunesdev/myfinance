1a254a2d6237967458c39d6c9c8cfc1f4af017a6
Fernando Antunes <f.antunes84@gmail.com>
2025-03-17 18:50:53 -0300

    ⚰️ Remoção deadcode Installment

--- statement/entities/installment.py
+++
@@ -1,8 +0,0 @@
-class Installment:
-    def __init__(self, release_date, description, user):
-        self.release_date = release_date
-        self.description = description
-        self.user = user
-
-    def __str__(self) -> str:
-        self.description
