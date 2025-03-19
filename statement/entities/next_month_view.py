71c6747855602347c0579f8d13874a8593e65b72
Fernando Antunes <f.antunes84@gmail.com>
2025-03-18 23:04:47 -0300

    ⚰️ Remoção de deadcode fixed_expense, next^month_view, fixed_income, dashboards, index, security

--- statement/entities/next_month_view.py
+++
@@ -1,8 +0,0 @@
-class NextMonthView:
-    def __init__(self, day, active, user):
-        self.day = day
-        self.active = active
-        self.user = user
-
-    def __str__(self) -> str:
-        return self.active
