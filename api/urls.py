5c1ac7a104abec438eaa1b49a8aedc93919ea79b
Fernando Antunes <f.antunes84@gmail.com>
2025-03-28 19:17:11 -0300

    ✨ Implementação do login por JWT

--- api/urls.py
+++ api/urls.py
@@ -1,19 +1,25 @@
 from django.urls import path
 
-from api.views.account_views import *
-from api.views.bank_views import *
-from api.views.card_views import *
-from api.views.category_views import *
-from api.views.default_views import *
-from api.views.extract_views import *
-from api.views.invoice_views import *
+from api.views.account_views import AccountsDetails, AccountsList
+from api.views.authentication import AuthenticationView
+from api.views.bank_views import BankDetails, BankList
+from api.views.card_views import CardDetails, CardList
+from api.views.category_views import CategoryDetails, CategoryList, CategoryType
+from api.views.default_views import Defaults
+from api.views.extract_views import ExtractByAccountYearAndMonth
+from api.views.invoice_views import InvoiceByCardYearAndMonth
 from api.views.next_month_view_view import NextMonthView
-from api.views.portfolio.fixed_income_views import *
-from api.views.subcategoy_views import *
-from api.views.transaction_views import *
+from api.views.portfolio.fixed_income_views import FixedIncomeProgressionList
+from api.views.subcategoy_views import SubcategoriesByCategory, SubcategoryList
+from api.views.transaction_views import TransactionsList, TransactionYear, TransactionByYearAndMonth, ImportTransactions
+
+authentication_view = AuthenticationView()
 
 urlpatterns = [
+    # Perfil
     path('next_month_view/', NextMonthView.as_view(), name='next_month_view'),
+
+    # Core
     path('accounts/', AccountsList.as_view()),
     path('accounts/<int:account_id>/', AccountsDetails.as_view()),
     path('banks/', BankList.as_view()),
@@ -28,18 +34,18 @@ urlpatterns = [
     path('transactions/', TransactionsList.as_view(), name='transactions'),
     path('transactions/year/<int:year>/', TransactionYear.as_view(), name='transaction-year'),
     path('transactions/year/<int:year>/month/<int:month>/', TransactionByYearAndMonth.as_view()),
-    path(
-        'transactions/accounts/<int:account_id>/year/<int:year>/month/<int:month>/',
-        ExtractByAccountYearAndMonth.as_view(),
-    ),
-    path(
-        'transactions/card/<int:card_id>/year/<int:year>/month/<int:month>/',
-        InvoiceByCardYearAndMonth.as_view(),
-    ),
+    path('transactions/accounts/<int:account_id>/year/<int:year>/month/<int:month>/', ExtractByAccountYearAndMonth.as_view()),
+    path('transactions/card/<int:card_id>/year/<int:year>/month/<int:month>/', InvoiceByCardYearAndMonth.as_view()),
     path('transactions/import/', ImportTransactions.as_view(), name='import-transactions'),
     path('transactions/', TransactionsList.as_view(), name='transactions-list'),
     path('subcategories/', SubcategoryList.as_view(), name='subcategory-list'),
+
+    # Retorno de configurações default para o FrontEnd antes de autenticar
     path('defaults/', Defaults.as_view(), name='defaults'),
+
     # Portfolio
     path('fixed-income/progression/', FixedIncomeProgressionList.as_view(), name='fixed_income_progression'),
+
+    # Autenticação JWT
+    path('token/', authentication_view.get_token, name='get_token'),
 ]
